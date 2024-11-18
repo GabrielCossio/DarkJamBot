7e99f1fb872b9947b7f3b59f1739dee6f1edae2e
trickkyricky <rickygyoungjr@gmail.com>
2024-11-18 14:17:17 -0600

    started spring boot app

---
+++ src/main/java/darkjam/example/darkjam/DarkjamApplication.java
@@ -0,0 +1,13 @@
+package darkjam.example.darkjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkjamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DarkjamApplication.class, args);
+    System.err.println("lol");
+  }
+}
---
+++ src/test/java/darkjam/example/darkjam/DarkjamApplicationTests.java
@@ -0,0 +1,13 @@
+package darkjam.example.darkjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkjamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
